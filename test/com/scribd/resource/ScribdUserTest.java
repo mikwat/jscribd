74d30877d1fe41b9fe15328baea372641e4c4b88
Mike Watts <mikwat@gmail.com>
2010-09-02 16:47:05 -0700

    Additional unit test.

--- test/com/scribd/resource/ScribdUserTest.java
+++ test/com/scribd/resource/ScribdUserTest.java
@@ -138,4 +138,51 @@ public class ScribdUserTest {
 		assertEquals("My Collection", list.get(0).getAttribute("collection_name"));
 		assertEquals("5", list.get(0).getAttribute("doc_count"));
 	}
+
+	@Test
+	public void getDocuments() {
+		MockApi mockApi = new MockApi() {
+			@Override
+			public Document sendRequest(String method, Map<String, Object> fields) {
+				try {
+					sendRequestCount++;
+					return dBuilder.parse(new ByteArrayInputStream(
+						("<rsp stat=\"ok\">" +
+							"<resultset totalResultsAvailable=\"922\" totalResultsReturned=\"2\" firstResultPosition=\"1\" list=\"true\">" +
+								"<result>" +
+									"<title><![CDATA[Ruby on Java]]></title>" +
+									"<description><![CDATA[Ruby On Java, Barcamp, Washington DC]]></description>" +
+									"<access_key>key-t3q5qujoj525yun8gf7</access_key>" +
+									"<doc_id>244565</doc_id>" +
+									"<page_count>10</page_count>" +
+									"<download_formats></download_formats>" +
+									"<when_uploaded>2009-09-02T18:24:12-05:00</when_uploaded>" +
+									"<when_updated>2010-08-02T18:24:12-05:00</when_updated>" +
+								"</result>" +
+								"<result>" +
+									"<title><![CDATA[Ruby on Java Part II]]></title>" +
+									"<description><![CDATA[Ruby On Java Part II, Barcamp, Washington DC]]></description>" +
+									"<access_key>key-2b3udhalycthsm91d1ps</access_key>" +
+									"<doc_id>244567</doc_id>" +
+									"<page_count>12</page_count>" +
+									"<download_formats>pdf,txt</download_formats>" +
+								"</result>" +
+							"</resultset>" +
+						"</rsp>").getBytes()));
+				} catch (Exception e) {
+					throw new RuntimeException(e);
+				}
+			}
+		};
+		scribd = new Scribd(mockApi);
+
+		ScribdUser user = buildUser();
+		List<ScribdDocument> list = user.getDocuments();
+
+		assertEquals(1, mockApi.sendRequestCount);
+		assertEquals(2, list.size());
+		assertEquals("244565", list.get(0).getAttribute("doc_id"));
+		assertEquals("Ruby on Java", list.get(0).getAttribute("title"));
+		assertEquals("10", list.get(0).getAttribute("page_count"));
+	}
 }
